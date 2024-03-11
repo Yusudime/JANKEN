892931cf8d0848c1af29608590db666d22b5d925
Yusudime <yusudimelucasnascimentosousa@gmail.com>
2024-03-11 10:04:00 -0300

    subindo alterações

--- junken 2.js
+++ junken 2.js
@@ -2,48 +2,55 @@ const PEDRA = "Pedra";
 const TESOURA = "Tesoura";
 const PAPEL = "Papel";
 
-function escolhaRandomica(){
-    return[PEDRA,TESOURA,PAPEL] [Math.floor(Math.random() * 3)];
+function escolhaRandomica() {
+    return [PEDRA, TESOURA, PAPEL][Math.floor(Math.random() * 3)];
 };
 
-class Jogador{
-    constructor(nome = "npc"){
+
+class Jogador {
+    constructor(nome = "npc") {
         this.nome = nome;
         this.vitorias = 0;
         this._escolha = null;
     }
-    get escolha() {return this._escolha};
-    set escolha(valor) {this._escolha = [PEDRA,TESOURA,PAPEL].includes(valor) ? valor:escolhaRandomica()};
-}const p1 = new Jogador("Lucas");
+    get escolha() { return this._escolha};
+    set escolha(valor) { this._escolha = [PEDRA, TESOURA, PAPEL].includes(valor) ? valor : PEDRA };
+}
+
+const p1 = new Jogador("Bruno");
 const npc = new Jogador();
 
-npc.escolher = function(){
+
+npc.escolher = function() {
     this.escolha = escolhaRandomica();
 };
-((nome = "Chico") => console.log(`Sejam bem vindo ${nome}. A partida ira começar`))(p1.nome);
 
-console.log("É a sua vez escolha entre essas 3 opções:");
-console.log("P -> Papel");
+
+((nome = "Chico") => console.log(`Seja bem vindo ${nome}. A partida vai começar`))(p1.nome);
+
+console.log("É sua vez. Digite:");
+console.log("P -> Pedra");
 console.log("T -> Tesoura");
-console.log("L -> Pedra");
+console.log("L -> Papel");
 
-function verificaEscolhar(escolha){
-    switch(escolha){
+function verificaEscolha(escolha) {
+    switch (escolha) {
         case 'P':
-            return PAPEL;
+            return PEDRA;
         case 'T':
             return TESOURA;
         case 'L':
-            return PEDRA;
+            return PAPEL;
         default:
             return false;
     }
 }
+
 const deuEmpate = (escolhaJogador, escolhaNPC) => escolhaJogador == escolhaNPC;
 
-const venceu = function(escolhaJogador, escolhaNPC){
-    if(
-        (escolhaJogador == PEDRA && escolhaNPC ==  TESOURA) ||
+const venceu = function (escolhaJogador, escolhaNPC) {
+    if (
+        (escolhaJogador == PEDRA && escolhaNPC == TESOURA) ||
         (escolhaJogador == TESOURA && escolhaNPC == PAPEL) ||
         (escolhaJogador == PAPEL && escolhaNPC == PEDRA)
     ){
@@ -51,26 +58,27 @@ const venceu = function(escolhaJogador, escolhaNPC){
     }
     return false;
 }
+
 const imprimeResultado = (vitoriasJogador, vitoriasNPC) => {
-    if (vitoriasJogador> vitoriasNPC){
-        console.log(`Parabén vc ganhou a melhor de 5, por ${vitoriasJogador} a ${vitoriasNPC}` );
-    }
-    else{
-        console.log(`Vc perdeu a melhor de 5, por ${vitoriasJogador} a ${vitoriasNPC}`);
+    if(vitoriasJogador > vitoriasNPC) {
+        console.log(`Parabéns vc venceu a melhor de 3, por ${vitoriasJogador} a ${vitoriasNPC}`);
+    } else {
+        console.log(`Infezlimente vc perdeu a melhor de 3, de ${vitoriasNPC} a ${vitoriasJogador}`);
     }
 }
-do{
+
+do {
     p1.escolha = PEDRA;
     npc.escolher();
-    if (deuEmpate(p1.escolha, npc.escolher)){
-        console.log(`Empate. Vc e o npc escolharam ${p1.escolha}`);
+
+    if (deuEmpate(p1.escolha, npc.escolha)) {
+        console.log(`Empate. Vc e o computdor escolheram ${p1.escolha}`);
     } else {
-        const venci = venceu(p1.escolha, npc.escolher);
-        const resultado = venci? "venceu" : "Perdeu";
-        venci? p1.vitorias++ : npc.vitorias++;
-        console.log(`Vc ${resultado}. Vc escolheu ${p1.escolha} e o computador escolheu ${npc.escolha}`);
+        const venci = venceu(p1.escolha, npc.escolha);
+        const resultado = venci ? "venceu" : "perdeu";
+        venci ? p1.vitorias++ : npc.vitorias++;
+        console.log(`Vc ${resultado}. Vc escolheu ${p1.escolha} e o computador escolheu ${npc.escolha}.`);
     }
-} while (Math.max(p1.vitorias, npc.vitorias)<3);
+} while (Math.max(p1.vitorias, npc.vitorias) < 3);
 
-imprimeResultado(p1.vitorias,npc.vitorias);
-
+imprimeResultado(p1.vitorias, npc.vitorias);
